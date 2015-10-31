63153a7d43137baddcf7ec460ee061af7fffd889
Estelle <estelleccl@gmail.com>
2015-10-31 15:27:13 +0800

    move UIbutton out

--- HelloWorld/HelloViewController.swift
+++ HelloWorld/HelloViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class HelloViewController: UIViewController {
-    var helloLabel: UILabel?
+    var helloLabel: UILabel!
+    var hiButton: UIButton!
+    var byeButton: UIButton!
+//    var helloLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +22,23 @@ class HelloViewController: UIViewController {
         
 //        let helloLabel: UILabel = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 150))
         helloLabel = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 150))
-        helloLabel!.text = "Hello World"
-        helloLabel!.backgroundColor = UIColor.blueColor()
+//        helloLabel!.text = "Hello World"
+//        helloLabel!.backgroundColor = UIColor.blueColor()
+        helloLabel.text = "Hello World"
+        helloLabel.backgroundColor = UIColor.blueColor()
+
         
         self.view.addSubview(helloLabel!)
         
-        let hiButton: UIButton = UIButton(frame: CGRect(x: 20, y: 250, width: 100, height: 30))
+//        let hiButton: UIButton = UIButton(frame: CGRect(x: 20, y: 250, width: 100, height: 30))
+        hiButton = UIButton(frame: CGRect(x: 20, y: 250, width: 100, height: 30))
         hiButton.addTarget(self, action: "sayhi", forControlEvents: UIControlEvents.TouchUpInside)
         hiButton.setTitle("Hi", forState: UIControlState.Normal)
         
         self.view.addSubview(hiButton)
         
-        let byeButton: UIButton = UIButton(frame: CGRect(x: 250, y: 250, width: 100, height: 30))
+//        let byeButton: UIButton = UIButton(frame: CGRect(x: 250, y: 250, width: 100, height: 30))
+        byeButton = UIButton(frame: CGRect(x: 250, y: 250, width: 100, height: 30))
         byeButton.addTarget(self, action: "saybye", forControlEvents: UIControlEvents.TouchUpInside)
         byeButton.setTitle("Bye", forState: UIControlState.Normal)
         
