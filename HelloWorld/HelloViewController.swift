b7f72fbd1d9c4ef255b37de11d34a6808a687de1
Estelle <estelleccl@gmail.com>
2015-10-31 15:04:46 +0800

    Hooked up button to change the words for Hello World label.

--- HelloWorld/HelloViewController.swift
+++ HelloWorld/HelloViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class HelloViewController: UIViewController {
+    var helloLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,18 +17,21 @@ class HelloViewController: UIViewController {
         //Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.lightGrayColor()
         
-        let helloLabel: UILabel = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 150))
-        helloLabel.text = "Hello World"
-        helloLabel.backgroundColor = UIColor.blueColor()
+//        let helloLabel: UILabel = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 150))
+        helloLabel = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 150))
+        helloLabel!.text = "Hello World"
+        helloLabel!.backgroundColor = UIColor.blueColor()
         
-        self.view.addSubview(helloLabel)
+        self.view.addSubview(helloLabel!)
         
         let hiButton: UIButton = UIButton(frame: CGRect(x: 20, y: 250, width: 100, height: 30))
+        hiButton.addTarget(self, action: "sayhi", forControlEvents: UIControlEvents.TouchUpInside)
         hiButton.setTitle("Hi", forState: UIControlState.Normal)
         
         self.view.addSubview(hiButton)
         
         let byeButton: UIButton = UIButton(frame: CGRect(x: 250, y: 250, width: 100, height: 30))
+        byeButton.addTarget(self, action: "saybye", forControlEvents: UIControlEvents.TouchUpInside)
         byeButton.setTitle("Bye", forState: UIControlState.Normal)
         
         self.view.addSubview(byeButton)
@@ -36,6 +40,16 @@ class HelloViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         //Dispose of any resources that can be recreated.
+    }
+    
+    func sayhi(){
+        NSLog("Say Hi")
+        helloLabel!.text = "hi again"
+    }
+    
+    func saybye(){
+        print("Say Bye")
+        helloLabel!.text = "bye bye"
     }
 
 }
